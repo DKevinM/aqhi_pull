470252e9bfecd9b1789b5cbb4ef537ff5351fb0f
DKevinM <35807537+DKevinM@users.noreply.github.com>
2025-05-19 23:37:06 -0600

    Update combine.py

--- combine.py
+++ combine.py
@@ -2,9 +2,26 @@ import pandas as pd
 
 # Define known station CSV URLs
 station_files = [
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Edmonton McCauley.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/St. Albert.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Woodcroft.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Edmonton East.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Edmonton Lendrum.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Ardrossan.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Sherwood Park.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/O’Morrow Station 1.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Poacher’s Landing Station 2.csv",
+    "https://github.com/DKevinM/aqhi_pull/blob/main/data/ACA/Leduc Sensor.csv",
+    
     "https://raw.githubusercontent.com/DKevinM/aqhi_pull/new/main/data/ACA/Edmonton_McCauley.csv",
     "https://raw.githubusercontent.com/youruser/yourrepo/main/data/ACA/Woodcroft.csv",
     "https://raw.githubusercontent.com/youruser/yourrepo/main/data/WCAS/St_Albert.csv",
+
+StationName = c("", "", "", "", "",
+                  "", "", "", "", "",
+                  "Breton", "Carrot Creek", "Drayton Valley", "Edson", "Genesee", "Hinton-Drinnan", "Meadows", 
+                  "Powers", "Steeper", "Wagner2", "Hinton-Hillcrest", "Jasper", "Enoch"),
+    
     "https://raw.githubusercontent.com/youruser/yourrepo/main/data/WCAS/Carrot_Creek.csv",
     # Add all 23 here manually or dynamically generate
 ]
